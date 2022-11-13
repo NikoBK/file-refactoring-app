ccda4c8797f02e14d5efc42bbeb758b08b7b1a0c
Nikolaj Bjørnager Krebs <112914618+NikoBK@users.noreply.github.com>
2022-11-13 01:49:39 +0100

    Page steps

--- Program.cs
+++ Program.cs
@@ -11,9 +11,16 @@ class Program
     private static bool _exit = false;
     public static PageTypes CurrentPage = PageTypes.None;
 
+    // Most pages have steps for processes.
+    private static int _currentPageStep = 1;
+
+    // SubCmd means whether we are writing a value using a command within a command or not.
+    private static bool _writingSubCmd = false;
+
     // Key: command, Value: Command handler.
     private static Dictionary<string, CommandHandler>? _menuCommands;
     private static Dictionary<string, CommandHandler>? _commonCommands; // Can be used on any page.
+    private static Dictionary<string, CommandHandler>? _createFileCommands;
 
     static void Main(string[] args)
     {
@@ -27,7 +34,11 @@ class Program
 
         do
         {
-            Console.Write("Enter command: ");
+            bool showCursor = _writingSubCmd ? false : true;
+            if (showCursor) { 
+                Console.Write("Enter command: "); 
+            }
+
             input = Console.ReadLine();
             HandleInput(input);
             Thread.Sleep(250);
@@ -45,8 +56,9 @@ class Program
 
         // If the command input is not a common command.
         var commandMap = GetPageCommands();
-        if (commandMap.ContainsKey(commandInput)) {
-            commandMap[commandInput](commandInput);
+        string key = _writingSubCmd ? "enterValue" : commandInput;
+        if (commandMap.ContainsKey(key)) {
+            commandMap[key](commandInput);
         } else
         {
             WriteLineHighlight($"Command: '{commandInput}' did not exist in the command map for page: {CurrentPage}.");
@@ -77,6 +89,11 @@ class Program
         {
             { "cfile", CreateFile }
         };
+
+        _createFileCommands = new Dictionary<string, CommandHandler>()
+        {
+            { "enterValue", CreateFile }
+        };
     }
 
     private static Dictionary<string, CommandHandler> GetPageCommands()
@@ -85,6 +102,10 @@ class Program
         switch(CurrentPage) {
             case PageTypes.Menu:
                 ret = _menuCommands;
+                break;
+
+            case PageTypes.CreateFile:
+                ret = _createFileCommands;
                 break;
 
             default:
@@ -136,14 +157,44 @@ class Program
         foreach (string command in commandMap.Keys) {
             Console.WriteLine($"{command}");
         }
-        Console.WriteLine("help");
         WriteLineHighlight("\n~~~ End \n");
     }
 
     private static void CreateFile(string input)
     {
-        CurrentPage = PageTypes.CreateFile;
-        Console.WriteLine("\n ~~~ Creating a file:");
-        Console.WriteLine("Welcome to the menu for creating a file!");
+        if (CurrentPage != PageTypes.CreateFile) {
+            CurrentPage = PageTypes.CreateFile;
+        }
+
+        if (!_writingSubCmd && _currentPageStep == 1) {
+            Console.WriteLine("\n ~~~ Creating a file:");
+            Console.WriteLine("Welcome to the menu for creating a file!");
+        }
+
+        string rootPath = string.Empty;
+        
+        switch(_currentPageStep)
+        {
+            case 1:
+                var rootDesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/";
+                if (string.IsNullOrEmpty(rootPath)) 
+                {
+                    if (_writingSubCmd) {
+                        rootPath = rootDesktopPath + input;
+                        _writingSubCmd = false;
+                        _currentPageStep++;
+                        Console.WriteLine($"Root path set: {rootPath}");
+                        CreateFile("");
+                    } else {
+                        Console.Write($"Enter root path: {rootDesktopPath}");
+                        _writingSubCmd = true;
+                    }
+                }
+                break;
+
+            case 2:
+                Console.WriteLine("this is step 2.");
+                break;
+        }
     }
 }
